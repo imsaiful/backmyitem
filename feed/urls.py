296eb8d9bcaeae1e1b381835d66e7630379f07bb
imsaiful <saifulcseng@gmail.com>
2018-08-05 05:29:00 +0530

    add the report form and connect to the db

--- feed/urls.py
+++ feed/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 from . import views
 
-
-app_name='feed'
+app_name = 'feed'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^report$', views.ReportCreate.as_view(), name='report'),
-    url(r'^detail/(?P<pk>[0-9]+)/$', views.ReportDetail.as_view(), name='report'),
+    url(r'^detail/(?P<pk>[0-9]+)/$', views.ReportDetail.as_view(), name='detail'),
 ]
