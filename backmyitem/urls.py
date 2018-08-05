a359a520a5d4f25a80ff7a64e46640c3de310539
imsaiful <saifulcseng@gmail.com>
2018-08-06 03:48:58 +0530

    add the auth info for button

--- backmyitem/urls.py
+++ backmyitem/urls.py
@@ -17,9 +17,12 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf.urls.static import static
 from . import settings
+from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('feed.urls')),
+    url(r'^login/$', LogoutView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view, name='logout'),
 ]
 
 
