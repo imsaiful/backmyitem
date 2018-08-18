a1d7bb9804f2281c489c46e8506916c085c6a7bb
imsaiful <saifulcseng@gmail.com>
2018-08-19 03:48:06 +0530

    add rds

--- backmyitem/settings.py
+++ backmyitem/settings.py
@@ -32,12 +32,12 @@ ALLOWED_HOSTS = ['127.0.0.1', 'ec2-13-232-209-235.ap-south-1.compute.amazonaws.c
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'feed',
 ]
 
@@ -45,7 +45,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
