06d864b82e7549d24063aa8edf63b74bff8fb281
imsaiful <saifulcseng@gmail.com>
2018-08-24 21:55:18 +0530

    add bucket

--- backmyitem/settings.py
+++ backmyitem/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'feed',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -134,6 +135,23 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "..", "media")
 MEDIA_URL = '/media/'
 
+
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+AWS_ACCESS_KEY_ID = 'AKIAJ5QB7WGZ5BJOR74A'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'backmyitem'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'backmyitem.storage_backends.MediaStorage'
