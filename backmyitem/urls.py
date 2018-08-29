df1201e0a3cc04d31208dda92aca30b6c797636d
imsaiful <saifulcseng@gmail.com>
2018-08-30 01:58:31 +0530

    add social

--- backmyitem/urls.py
+++ backmyitem/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset/done/$', password_reset_complete, name='password_reset_complete'),
 
+
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
