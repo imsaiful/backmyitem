fc4b3bc60264dd004f20938c8f74a1734030bb0f
imsaiful <saifulcseng@gmail.com>
2018-09-15 00:00:27 +0530

    add contact form

--- feed/admin.py
+++ feed/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Report_item,UserNotification
+from .models import Report_item,UserNotification ,ContactHelp
 
 
 admin.site.register(Report_item)
 admin.site.register(UserNotification)
+admin.site.register(ContactHelp)
 
 
 
